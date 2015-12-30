f70a2d71be7c6e3137380805c501bb07b9069561
nan <nan.030539@gmail.com>
2015-12-30 10:25:30 +0700

    SQL fin

--- app/src/main/java/apprtc/pn/carbuddythailand/MainActivity.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //Explict
+    private ManaggeTABLE objManageTABLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Setup Constructor
+        objManageTABLE = new ManaggeTABLE(this);
+
+    }   //Main Method
+
+} //Main Call
