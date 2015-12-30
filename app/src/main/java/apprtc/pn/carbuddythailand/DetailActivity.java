681f0dcfd7f91431a3abb321bb1197fdea17ca1b
nan <nan.030539@gmail.com>
2015-12-30 14:57:46 +0700

    Select fin

--- app/src/main/java/apprtc/pn/carbuddythailand/DetailActivity.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/DetailActivity.java
@@ -2,12 +2,24 @@ package apprtc.pn.carbuddythailand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // Explicit
+    public String TAG = "Carbuddy";
+    private  String selecString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-}
+
+        // Receive Value From Intent
+        selecString = getIntent().getStringExtra("Select");
+        Log.d(TAG,"Select="+selecString);
+
+    } // Main Method
+
+}// Main Class
