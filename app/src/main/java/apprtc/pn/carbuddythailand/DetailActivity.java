72bd2784bead3d2d0151999127c9e5b17b62398b
nan <nan.030539@gmail.com>
2015-12-30 15:55:48 +0700

    Crate Custom ListView

--- app/src/main/java/apprtc/pn/carbuddythailand/DetailActivity.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/DetailActivity.java
@@ -1,8 +1,11 @@
 package apprtc.pn.carbuddythailand;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,6 +23,35 @@ public class DetailActivity extends AppCompatActivity {
         selecString = getIntent().getStringExtra("Select");
         Log.d(TAG,"Select="+selecString);
 
+        //ListView Controller
+        ListViewController();
+
+
     } // Main Method
+
+    private void ListViewController() {
+
+        //Get Data From Database
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM phoneTABLE WHERE Category = " + "'" + selecString + "'" , null);
+
+        objCursor.moveToFirst();
+        String[] nameStrings = new String[objCursor.getCount()];
+        String[] phoneStrings = new String[objCursor.getCount()];
+
+        for (int i = 0; i < objCursor.getCount(); i++) {
+            nameStrings[i] = objCursor.getString(objCursor.getColumnIndex(ManaggeTABLE.COLUMN_NAME));
+            phoneStrings[i] = objCursor.getString(objCursor.getColumnIndex(ManaggeTABLE.COLUMN_PHONE));
+            objCursor.moveToNext();
+        }   //for
+        objCursor.close();
+
+        //Create ListView
+        MyAdapter objMyAdapter = new MyAdapter(DetailActivity.this, phoneStrings, nameStrings);
+        ListView nameListView = (ListView) findViewById(R.id.listView);
+        nameListView.setAdapter(objMyAdapter);
+
+    }   //ListViewConttorller
 
 }// Main Class
---
+++ app/src/main/java/apprtc/pn/carbuddythailand/MyAdapter.java
@@ -0,0 +1,54 @@
+package apprtc.pn.carbuddythailand;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by com on 30/12/2558.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private String[] nameStrings, phoneStrings;
+
+    public MyAdapter(Context objContext, String[] phoneStrings, String[] nameStrings) {
+        this.objContext = objContext;
+        this.phoneStrings = phoneStrings;
+        this.nameStrings = nameStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.phone_listview, viewGroup, false);
+
+        TextView nameTextView = (TextView) objView1.findViewById(R.id.textView);
+        nameTextView.setText(nameStrings[i]);
+
+        TextView phoneTextView = (TextView) objView1.findViewById(R.id.textView2);
+        phoneTextView.setText(phoneStrings[i]);
+
+        return objView1;
+    }
+}   //Main Class
