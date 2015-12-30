4d31672313a871f23d083318dbbaab22b6bed7e3
nan <nan.030539@gmail.com>
2015-12-30 16:58:25 +0700

    Finish 30 Dec 15

--- app/src/main/java/apprtc/pn/carbuddythailand/ManaggeTABLE.java
+++ app/src/main/java/apprtc/pn/carbuddythailand/ManaggeTABLE.java
@@ -2,6 +2,7 @@ package apprtc.pn.carbuddythailand;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -9,14 +10,14 @@ import android.database.sqlite.SQLiteDatabase;
  */
 public class ManaggeTABLE {
     //Expilcit
-    private  MyOpenHelper objMyOpenHelper;
+    private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
-    public static final String TABLE_PHONE ="phoneTABLE";
-    public static final String COLUMN_ID ="_id";
+    public static final String TABLE_PHONE = "phoneTABLE";
+    public static final String COLUMN_ID = "_id";
     public static final String COLUMN_CATEGORY = "Category";
-    public static final String COLUMN_NAME ="Name";
-    public static final String COLUMN_PHONE ="Phone";
+    public static final String COLUMN_NAME = "Name";
+    public static final String COLUMN_PHONE = "Phone";
 
     public ManaggeTABLE(Context context) {
 
@@ -27,6 +28,40 @@ public class ManaggeTABLE {
 
     }   //Constructor
 
+    public String[] searchName(String StrName) {
+
+        try {
+
+            String[] resultStrings = null;
+            Cursor objCursor = readSqLiteDatabase.query(TABLE_PHONE,
+                    new String[]{COLUMN_ID, COLUMN_CATEGORY, COLUMN_NAME, COLUMN_PHONE},
+                    COLUMN_NAME + "=?",
+                    new String[]{String.valueOf(StrName)},
+                    null, null, null, null);
+
+            if (objCursor != null) {
+                if (objCursor.moveToFirst()) {
+
+                    resultStrings = new String[4];
+                    resultStrings[0] = objCursor.getString(0);
+                    resultStrings[1] = objCursor.getString(1);
+                    resultStrings[2] = objCursor.getString(2);
+                    resultStrings[3] = objCursor.getString(3);
+
+
+                }//if
+            }   //if
+            objCursor.close();
+            return resultStrings;
+
+        } catch (Exception e) {
+            return null;
+
+        }
+
+        //return new String[0];
+    }
+
     //Add New Value to SQLite
     public Long addNewValue(String StrCategory, String StrName, String strPhone) {
 
@@ -34,7 +69,7 @@ public class ManaggeTABLE {
         objContentValues.put(COLUMN_CATEGORY, StrCategory);
         objContentValues.put(COLUMN_NAME, StrName);
         objContentValues.put(COLUMN_PHONE, strPhone);
-        return writeSqLiteDatabase.insert(TABLE_PHONE, null,objContentValues);
+        return writeSqLiteDatabase.insert(TABLE_PHONE, null, objContentValues);
     }
 
 
