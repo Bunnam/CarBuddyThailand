cf63e548273e9bb6972cc11f231cf8ab60e808a5
nan <nan.030539@gmail.com>
2016-01-03 13:55:56 +0700

    หน้าแรก

---
+++ app/src/main/java/apprtc/pn/carbuddythailand/SplashScreen.java
@@ -0,0 +1,31 @@
+package apprtc.pn.carbuddythailand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by com on 3/1/2559.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_splash_screen);
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent obIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(obIntent);
+                finish();
+
+            }
+        }, 8000);
+        //MediaPlayer intrMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+
+    }//Main Method
+} //Main Class
